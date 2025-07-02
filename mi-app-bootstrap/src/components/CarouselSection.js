6b722c8fe57a8ef122b6ac30ce6bbf43d559407e
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-07-01 20:51:43 -0500

    Avance 2

--- mi-app-bootstrap/src/components/CarouselSection.js
+++ mi-app-bootstrap/src/components/CarouselSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import imagen from '../assets/imagen.jpg'; // usa tu imagen aquí
+import imagen from '../assets/imagen.jpg';
 
 function CarouselSection() {
   return (
